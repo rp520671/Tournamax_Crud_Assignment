1930ee4b3961484ae0f333e9c6cfab385ed42c75
rp520671 <rp520671@gmail.com>
2024-08-17 15:47:51 +0530

    new

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
+let isConnected = false; // Track the connection status
+
 const connectMongoDB = async () => {
+  if (isConnected) {
+    console.log("MongoDB is already connected.");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = true;
     console.log("Connected to MongoDB.");
   } catch (error) {
-    console.log(error);
+    console.log("MongoDB connection error:", error);
+    throw new Error("MongoDB connection failed");
   }
 };
 
